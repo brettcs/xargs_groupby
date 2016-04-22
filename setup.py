4fa52ac9986062eac0270c2edb12abbaf9e34c30
Brett Smith <brettcsmith@brettcsmith.org>
2016-04-21 22:32:06 -0400

    Add test infrastructure.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from setuptools import setup
+
+setup(name='xargs_groupby',
+      version='0.1',
+      test_suite='tests')
